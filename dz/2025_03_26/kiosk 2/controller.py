23bb02666b78ed399e5c802968c81b55b4c64ffd
Sergey319 <sergej.shevelev.@list.ru>
2025-04-01 20:18:53 +0300

    2025-03-26 kiosk2.4

--- dz/2025_03_26/kiosk 2/controller.py
+++ dz/2025_03_26/kiosk 2/controller.py
@@ -1,12 +1,18 @@
-from model import *
+
 from view import *
 
 class OrdersController:
     def add_hd_in_order(self, orders, hd, reserve):
+        order = True
         for ingredient in hd.ingredients:
-            if 0 < reserve.get_count_ingredient_in_reserve(ingredient) <= 3:
+            if reserve.get_count_ingredient_in_reserve(ingredient) > 3:
+                reserve.sub_reserve(ingredient)
+            elif 0 < reserve.get_count_ingredient_in_reserve(ingredient) <= 3:
                 print(f"!!!!! Ингредиент: {ingredient.name} осталось {reserve.get_count_ingredient_in_reserve(ingredient)} штук !!!!!")
-        orders.add_orders(hd)
-        for ingredient in hd.ingredients:
-            reserve.sub_reserve(ingredient)
+                reserve.sub_reserve(ingredient)
+            elif reserve.get_count_ingredient_in_reserve(ingredient) == 0:
+                print(f"!!!!!!!!!!! Ингредиента {ingredient.name} нет !!!!!!!!!!!")
+                order = False
+        if order == True:
+            orders.add_orders(hd)
         OrderView.display_order(orders)
