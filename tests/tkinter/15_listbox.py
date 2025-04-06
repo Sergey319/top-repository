f58736cdd51681b300904d54f1b3cc3b9d53ecd8
Sergey319 <sergej.shevelev.@list.ru>
2025-04-06 15:37:31 +0300

    15

--- tests/tkinter/15_listbox.py
+++ tests/tkinter/15_listbox.py
@@ -31,3 +31,51 @@ from tkinter import ttk
 #languages_listbox.pack(anchor=NW, fill=X, padx=5, pady=5)
 #
 #root.mainloop()
+"""Для наполнения listboxа элементами определяем список languages, затем этот список передаем в переменную типа Variable. Затем привязываем эту переменную к параметру listvariable у Listbox"""
+"""Основные методы Listbox"""
+"""Listbox имеет ряд методов для управления поведением элемента и его содержимым. Некоторые из них:"""
+# curselection(): возвращает набор индексов выделенных элементов
+# delete(first, last = None): удаляет элементы с индексами из диапазона [first, last]. Если второй параметр опущен, то удаляет только один элемент по индексу first
+# get(first, last = None): возвращает кортеж, который содержит текст элементов с индексами из диапазона [first, last]. Если второй параметр опущен, возвращается только текст элемента с индексом first
+# insert(index, element): вставляет элемент по определенному индексу
+# size(): возвращает количество элементов
+"""Для рассмотрения этих методов напишем небольшой скрипт по управлению данными:"""
+## удаление выделенного элемента
+#def delete():
+#    selection = languages_listbox.curselection()
+#    # мы можем получить удаляемый элемент по индексу
+#    # selected_language = languages_listbox.get(selection[0])
+#    languages_listbox.delete(selection[0])
+#
+## добавление нового элемента
+#def add():
+#    new_language = language_entry.get()
+#    languages_listbox.insert(0, new_language)
+#
+#root = Tk()
+#root.title("METANIT.COM")
+#root.geometry("300x250")
+#root.columnconfigure(index=0, weight=4)
+#root.columnconfigure(index=1, weight=1)
+#root.rowconfigure(index=0, weight=1)
+#root.rowconfigure(index=1, weight=3)
+#root.rowconfigure(index=2, weight=1)
+#
+## текстовое поле и кнопка для добавления в список
+#language_entry = ttk.Entry()
+#language_entry.grid(column=0, row=0, padx=6, pady=6, sticky=EW)
+#ttk.Button(text="Добавить", command=add).grid(column=1, row=0, padx=6, pady=6)
+#
+## создаем список
+#languages_listbox = Listbox()
+#languages_listbox.grid(row=1, column=0, columnspan=2, sticky=EW, padx=5, pady=5)
+#
+## добавляем в список начальные элементы
+#languages_listbox.insert(END, "Python")
+#languages_listbox.insert(END, "C#")
+#
+#ttk.Button(text="Удалить", command=delete).grid(row=2, column=1, padx=5, pady=5)
+#
+#root.mainloop()
+"""Для манипулирования элемента списка здесь определенно две кнопки. Первая кнопка вызывает функцию add(), которая получает введенное в текстовое поле значение и добавляет его на первое место в списке с помощью метода insert()."""
+"""Вторая кнопка по нажатию удаляет выделенный элемент. Для этого мы сначала получаем выделенные индексы через метод curselection(). Так как в нашем случае выделяется только один элемент, то получаем его индекс через выражение selection[0]. И этот индекс передаем в метод delete() для удаления."""
