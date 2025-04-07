04bd30a89be0d1d1675bd5ef7554b64319cac442
Sergey319 <sergej.shevelev.@list.ru>
2025-04-07 18:58:52 +0300

    17

--- tests/tkinter/17_combobox.py
+++ tests/tkinter/17_combobox.py
@@ -1,3 +1,5 @@
+#from cProfile import label
+#from cgitb import text
 from tkinter import *
 from tkinter import ttk
 
@@ -36,5 +38,57 @@ Entry и Listbox
 #
 #root.mainloop()
 """
-
+Здесь для элемента combobox в качестве источника значений устанавливается
+список languages:
 """
+"""
+С помощью параметра textvariable мы можем установить привязку к выбранному
+в Combobox значению:
+"""
+#root = Tk()
+#root.title("METANIT.COM")
+#root.geometry("250x200")
+#
+#languages = ["Python", "C#", "Java", "JavaScript"]
+## по умолчанию будет выбран первый элемент из languages
+#languages_var = StringVar(value=languages[0])
+#
+#label = ttk.Label(textvariable=languages_var)
+#label.pack(anchor=NW, padx=6, pady=6)
+#
+#combobox = ttk.Combobox(textvariable=languages_var, values=languages)
+#combobox.pack(anchor=NW, padx=6, pady=6)
+#
+#print(combobox.get())
+#root.mainloop()
+"""
+Здесь выбранный в Combobox элемент привязан к переменной languages_var. По
+умолчанию выбран первый элемент списка languages. Для отслеживания изменения
+выбора определена метка Label, которая отображает выбранный элемент:
+"""
+"""
+По умолчанию мы можем ввести в текстовое поле в Combobox любое значение,
+даже то, которого нет в списке. Такое поведение не всегда может быть удобно. В
+этом случае мы можем установить для виджета состояние только для чтения,
+передав параметру "state" значение "readonly":
+"""
+#combobox = ttk.Combobox(textvariable=languages_var, values=languages, state="readonly")
+"""
+Выбранный элемент можно получить с помощью метода get() класса Combobox
+"""
+#selection = combobox.get()
+"""
+Либо с помощью метода get() привязанной переменной
+"""
+#selection = languages_var.get()
+"""
+Для установки нового значения можно использовать метод set():
+"""
+#languages_var.set(new_value)
+#combobox.set(new_value)
+"""
+Для установки по идексу из привязанного набора значений также можно
+использовать метод current(newindex), где с помощью параметра newindex
+задается индекс выбранного значения. Например, выберем второй элемент:
+"""
+#combobox.current(1)
