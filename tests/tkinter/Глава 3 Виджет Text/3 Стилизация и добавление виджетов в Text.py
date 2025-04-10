6c0936dbc40364e379693b7665ebd49f3a4401d5
Sergey319 <sergej.shevelev.@list.ru>
2025-04-10 19:24:21 +0300

    3.3

--- tests/tkinter/Глава 3 Виджет Text/3 Стилизация и добавление виджетов в Text.py
+++ tests/tkinter/Глава 3 Виджет Text/3 Стилизация и добавление виджетов в Text.py
@@ -34,3 +34,80 @@ spacing2, spacing3, tabs, tabstyle, underline и wrap, которым перед
 
 Посмотрим на примере:
 """
+#root = Tk()
+#root.title("METANIT.COM")
+#root.geometry("250x200")
+#
+#editor = Text(wrap="none")
+#editor.pack(expand=1, fill=BOTH)
+#editor.insert("1.0", "Hello ")
+## создаем тег highlightline и прикрепляем его к символам 1.0 до 1.2
+#editor.tag_add("highlightline", "1.0", "1.2")
+## добавляем текст, к которому применяется тег highlightline
+#editor.insert("end", "World", "highlightline")
+#editor.insert("end", "\nHello All!")
+## устанавливаем стили тега highlightline
+#editor.tag_config("highlightline", background="#ccc", foreground="red", font="TkFixedFont", relief="raised")
+#
+#root.mainloop()
+"""
+Здесь создается тег "highlightline", который прикрепляется сначала по 2-й символ в
+первой строке. Далее добавляется текст "World", к которому применяется данный тег.
+В конце конфигурируем тег, задавая его стилевые параметры.
+
+Если в процессе программы тег стал не нужен, его можно удалить. Метод
+tag_remove() удаляет тег с определенных символов:
+
+editor.tag_remove("highlightline", "1.0", "1.2")
+
+В данном случае удаляем тег "highlightline" с символов с 0 по 2-й в первой строке.
+
+Также можно вообще удалить тег со всех символов, к которым он применяется:
+
+editor.tag_delete("highlightline")
+"""
+"""
+Добавление изображений и других виджетов
+
+Виджет Text позволяет добавление изображений и других виджетов.
+Для добавления изображений применяется метод image_create:
+"""
+#root = Tk()
+#root.title("METANIT.COM")
+#root.geometry("250x200")
+#
+#editor = Text()
+#editor.pack(expand=1, fill=BOTH)
+#
+#python_img = PhotoImage(file="python.png")
+#editor.image_create("1.0", image=python_img)
+#
+#root.mainloop()
+"""
+В метод image_create в качестве первого параметра передается позиция вставки
+изображения. В качестве второго параметра - image указывается файл
+изображения.
+
+Аналогично можно добавлять другие виджеты в Text с помощью метода
+window_create():
+"""
+#root = Tk()
+#root.title("METANIT.COM")
+#root.geometry("250x200")
+#
+#editor = Text()
+#editor.pack(expand=1, fill=BOTH)
+#
+#def click():
+#    editor.insert("2.0", "Click\n")
+#
+#btn = ttk.Button(editor, text="Click", command=click)
+#editor.window_create("1.0", window=btn)
+#
+#root.mainloop()
+"""
+Первый параметр метода window_create также позиция создания виджета, а
+второй параметр - window указывает на добавляемый виджет, в данном случае это
+кнопка, на которую также можно нажимать и также можно обрабатывать ее
+нажатие
+"""
