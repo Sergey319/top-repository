5659c4f22eedfa310f95f016a4274e9a546ffc4a
Sergey319 <sergej.shevelev.@list.ru>
2025-04-11 12:35:39 +0300

    4.2

--- tests/tkinter/Глава 4 Виджет Treeview Создание таблиц и деревьев/2 Создание таблиц.py
+++ tests/tkinter/Глава 4 Виджет Treeview Создание таблиц и деревьев/2 Создание таблиц.py
@@ -6,7 +6,7 @@ from tkinter import ttk
 """
 """
 Для отображения данных в виде таблицы параметру show предпочтительно
-передать значение "headings" (есло надо отображать заголовки), либо " " (для
+передать значение "headings" (если надо отображать заголовки), либо " " (для
 таблицы без заголовков). Определим небольшую таблицу с тремя столбцами:
 """
 #root = Tk()
@@ -150,5 +150,46 @@ column(column, width, minwidth, stretch, anchor)
 ## добавляем данные
 #for person in people:
 #    tree.insert("", END, values=person)
+#
+#root.mainloop()
+"""
+Добавление к Treeview прокрутки
+"""
+#root = Tk()
+#root.title("METANIT.COM")
+#root.geometry("250x200")
+#root.rowconfigure(index=0, weight=1)
+#root.columnconfigure(index=0, weight=1)
+#
+## определяем данные для отображения
+#people = [
+#    ("Tom", 38, "tom@email.com"), ("Bob", 42, "bob@email.com"), ("Sam", 28, "sam@email.com"),
+#    ("Alice", 33, "alice@email.com"), ("Kate", 21, "kate@email.com"), ("Ann", 24, "ann@email.com"),
+#    ("Mike", 34, "mike@email.com"), ("Alex", 52, "alex@email.com"), ("Jess", 28, "jess@email.com"),
+#    ]
+#
+## определяем столбцы
+#columns = ("name", "age", "email")
+#
+#tree = ttk.Treeview(columns=columns, show="headings")
+#tree.grid(row=0, column=0, sticky=NSEW)
+#
+## определяем заголовки
+#tree.heading("name", text="Имя", anchor=W)
+#tree.heading("age", text="Возраст", anchor=W)
+#tree.heading("email", text="Email", anchor=W)
+#
+#tree.column("#1", stretch=NO, width=70)
+#tree.column("#2", stretch=NO, width=60)
+#tree.column("#3", stretch=NO, width=100)
+#
+## добавляем данные
+#for person in people:
+#    tree.insert("", END, values=person)
+#
+## добавляем вертикальную прокрутку
+#scrollbar = ttk.Scrollbar(orient=VERTICAL, command=tree.yview)
+#tree.configure(yscroll=scrollbar.set)
+#scrollbar.grid(row=0, column=1, sticky=NS)
 #
 #root.mainloop()
