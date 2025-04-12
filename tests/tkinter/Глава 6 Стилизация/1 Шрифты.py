7c51a7fac05bce0bc6ce75d4e12197a6f412837b
Sergey319 <sergej.shevelev.@list.ru>
2025-04-12 20:21:49 +0300

    6,1

---
+++ tests/tkinter/Глава 6 Стилизация/1 Шрифты.py
@@ -0,0 +1,77 @@
+"""
+
+    Шрифты
+
+
+"""
+"""
+    Имена шрифтов
+    
+    Ряд виджетов, например, Label или Text, поддерживают установку шрифта через
+параметр font. Каждая платформа может определять свои специфические шрифты.
+Но также библиотека Tk по умолчанию включает ряд именованных шрифтов,
+которые могут использоваться на различных компонентах графического
+интерфейса и которые доступны на всех платформах:
+    * TkDefaultFont: шрифт по умолчанию, который применяется, если для виджета
+    явным образом не определен шрифт
+    * TkTextFont: шрифт по умолчанию, который применяется для виджетов Entry,
+    Listbox и ряда других
+    * TkFixedFont: шрифт с фиксированной шириной
+    * TkMenuFont: шрифт для пунктов меню
+    * TkHeadingFont: шрифт для заголовков в Listbox и в таблицах
+    * TkCaptionFont: шрифт для строки статуса в окнах
+    * TkSmallCaptionFont: шрифт малого размера для диалоговых окон
+    * TkIconFont: шрифт для подписей к иконкам
+    * TkTooltipFont: шрифт для всплывающих окон
+    В принципе мы можем использовать эти шрифты не только в любых виджетах:
+    ttk.Label(text="Hello World", font="TkTextFont")
+    Tk также предоставляет дополнительный набор именованных шрифтов, которые
+определены только на определенных платформах. Для их получения можно
+использовать функцию name() из пакета tkinter.font:
+"""
+#from tkinter import *
+#from tkinter import font
+#
+#root = Tk()
+#
+#for font_name in font.names():
+#    print(font_name)
+"""
+    Например, на Windows мы получим следующий набор:
+    fixed
+    oemfixed
+    TkDefaultFont
+    TkMenuFont
+    ansifixed
+    systemfixed
+    TkHeadingFont
+    device
+    TkTooltipFont
+    defaultgui
+    TkTextFont
+    ansi
+    TkCaptionFont
+    system
+    TkSmallCaptionFont
+    TkFixedFont
+    TkIconFont
+    В данном случае выводится и платформа-независимые, и платформа-специальные
+шрифты, например, "system".
+"""
+#from tkinter import *
+#from tkinter import ttk
+#
+#root = Tk()
+#root.title("METANIT.COM")
+#root.geometry("250x200")
+#
+#label = ttk.Label(text="Hello World", font="system")
+#label.pack(anchor=NW, expand=1)
+#
+#root.mainloop()
+"""
+    Определение шрифта
+    
+    За определение шрифта в Tkinter отвечает класс Font из модуля tkinter.font. Он
+принимает следующие параметры:
+"""
