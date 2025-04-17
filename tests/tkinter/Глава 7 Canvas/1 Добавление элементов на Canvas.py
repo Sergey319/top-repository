28659cabf351936b2a04c9c0bc7aedca4bc4c323
Sergey319 <sergej.shevelev.@list.ru>
2025-04-17 11:58:21 +0300

    7.2

--- tests/tkinter/Глава 7 Canvas/1 Добавление элементов на Canvas.py
+++ tests/tkinter/Глава 7 Canvas/1 Добавление элементов на Canvas.py
@@ -321,28 +321,28 @@ __y1 - конечной.
     Для создания прокрутки виджет Canvas предоставляет параметр, который
 позволяет установить прокручиваемую область:
 """
-from tkinter import *
-from tkinter import ttk
-
-root = Tk()
-root.title("METANIT.COM")
-root.geometry("250x200")
-
-h = ttk.Scrollbar(orient=HORIZONTAL)
-v = ttk.Scrollbar(orient=VERTICAL)
-canvas = Canvas(scrollregion=(0, 0, 1000, 1000), bg="white", yscrollcommand=v.set, xscrollcommand=h.set)
-h["command"] = canvas.xview
-v["command"] = canvas.yview
-
-canvas.grid(column=0, row=0, sticky=(N, W, E, S))
-h.grid(column=0, row=1, sticky=(W, E))
-v.grid(column=1, row=0, sticky=(N, S))
-root.grid_columnconfigure(0, weight=1)
-root.grid_rowconfigure(0, weight=1)
-
-canvas.create_rectangle(10, 10, 300, 300, fill="red")
-
-root.mainloop()
+#from tkinter import *
+#from tkinter import ttk
+#
+#root = Tk()
+#root.title("METANIT.COM")
+#root.geometry("250x200")
+#
+#h = ttk.Scrollbar(orient=HORIZONTAL)
+#v = ttk.Scrollbar(orient=VERTICAL)
+#canvas = Canvas(scrollregion=(0, 0, 1000, 1000), bg="white", yscrollcommand=v.set, xscrollcommand=h.set)
+#h["command"] = canvas.xview
+#v["command"] = canvas.yview
+#
+#canvas.grid(column=0, row=0, sticky=(N, W, E, S))
+#h.grid(column=0, row=1, sticky=(W, E))
+#v.grid(column=1, row=0, sticky=(N, S))
+#root.grid_columnconfigure(0, weight=1)
+#root.grid_rowconfigure(0, weight=1)
+#
+#canvas.create_rectangle(10, 10, 300, 300, fill="red")
+#
+#root.mainloop()
 """
     В данном случае устанавливается прокручиваемая область 1000х1000:
     canvas = Canvas(scrollregion=(0, 0, 1000, 1000), ....
